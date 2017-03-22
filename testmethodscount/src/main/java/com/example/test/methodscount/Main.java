e19186541b3c24f6953be56767fb6eaf051784b6
Victor <annimon119@gmail.com>
2017-03-22 12:38:51 +0200

    Add methods counter demo

---
+++ testmethodscount/src/main/java/com/example/test/methodscount/Main.java
@@ -0,0 +1,24 @@
+package com.example.test.methodscount;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    private void method1() {
+
+    }
+
+    protected String method2() {
+        return "";
+    }
+
+    public int method3(int x) {
+        return x + 1;
+    }
+
+    private static void method4() {
+
+    }
+}
---
+++ testmethodscount/src/main/java/com/example/test/methodscount/Test.java
@@ -0,0 +1,34 @@
+package com.example.test.methodscount;
+
+public class Test {
+
+    private void method1() {
+    }
+
+    private void method2() {
+    }
+
+    private void method3() {
+    }
+
+    private void method4() {
+    }
+
+    private void method5() {
+    }
+
+    private void method6() {
+    }
+
+    private void method7() {
+    }
+
+    private void method8() {
+    }
+
+    private void method9() {
+    }
+
+    private void method10() {
+    }
+}
